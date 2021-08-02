b2f0e67a7dede8d063a7fecf5e940ce0f111991a
Fuck <setsunaafie@protonmail.com>
2021-08-03 03:33:14 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - axie',
-    title: 'axie',
+    titleTemplate: '%s - Dashboard',
+    title: 'My Scholar Tracker',
     htmlAttrs: {
       lang: 'en'
     },
